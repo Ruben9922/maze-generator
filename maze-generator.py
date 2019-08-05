9364ce03ef6980809cb780ed12aba6755a6641a6
Ruben9922 <Ruben9922@users.noreply.github.com>
2019-08-05 22:17:30 +0100

    Use default values for width and height when left blank

--- maze-generator.py
+++ maze-generator.py
@@ -1,11 +1,12 @@
 import numpy as np
 
 
-def input_int(prompt, min=None, max=None):
+def input_int(prompt, default=None, min=None, max=None):
     input_valid = False
     while not input_valid:
+        s = input(f"{prompt} (leave blank for {default}): ")
         try:
-            number = int(input(prompt))
+            number = int(s)
             if (min is None or number >= min) and (max is None or number <= max):
                 input_valid = True
                 return number
@@ -17,8 +18,11 @@ def input_int(prompt, min=None, max=None):
                 else:
                     print(f"Integer less than or equal to {max} only!")
         except ValueError as e:
-            print("Integers only!")
+            if default is not None and s == "":
+                return default
+            else:
+                print("Integers only!")
 
 
-width = input_int("Width: ", 0, None)
-height = input_int("Height: ", 0, None)
+width = input_int("Width", 20, 0, None)
+height = input_int("Height", width, 0, None)
